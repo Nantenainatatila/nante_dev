274dd609d53b28344691a8b19e85779544cb6a16
Nantenainatatila <nantenainatatila@gmail.com>
2026-05-02 00:12:16 +0200

    Mis à jour

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask
-
 import os
 
+app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-app = Flask(__name__)
+
 
 @app.route("/")
 def home():
